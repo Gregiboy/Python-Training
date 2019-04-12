b52e0db33599a11159a890dce05364cafd2ed461
Gregory Van Hamme <van.hamme.gregory@gmail.com>
2019-04-12 22:29:00 +0200

    Initial release

--- GuessGame.py
+++ GuessGame.py
@@ -1,10 +1,21 @@
 #Number Guessing game: v0.01
 #by Gregory Van Hamme
 
-def number(4)
+from random import randint
+x = randint(1, 20)
+Guess = False
 
 print("Hello, Welcome to the my Number-Guessing game")
 print("I am thinking of a number from 1-20")
 print("Please take a guess")
-myGuess = input()
 
+while Guess == False:
+    myGuess = input()
+    if x > myGuess:
+        print ("Too Low!")
+    elif x < myGuess :
+        print ("Too High!")
+    else:
+        Guess = True
+        break
+print ("Nice Guess!")
