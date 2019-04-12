49bfe1bcb1df0293c913faaea077813429ef0eaf
Gregory Van Hamme <van.hamme.gregory@gmail.com>
2019-04-12 16:39:03 +0200

    Add files via upload
    Initial version.

---
+++ GuessGame.py
@@ -0,0 +1,10 @@
+#Number Guessing game: v0.01
+#by Gregory Van Hamme
+
+def number(4)
+
+print("Hello, Welcome to the my Number-Guessing game")
+print("I am thinking of a number from 1-20")
+print("Please take a guess")
+myGuess = input()
+
